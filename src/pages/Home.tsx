3117d3a094d8d3d48f7f09c5c7f5177601410b93
samueldesmit <samuel_desmit@hotmail.com>
2026-02-03 23:14:28 +0100

    added scroll lock

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import Logo3D from '../components/Logo3D';
 import './Home.css';
 
 export default function Home() {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
   return (
     <div className="hero">
       <div className="hero-bg-image" aria-hidden="true" />
